90ddda05c9ffe1e4481ac785e599e683f6a090f3
Mickael Rebeau <mrebeau@productlife-group.com>
2025-05-18 19:14:04 +0200

    Ajout du composant Header.vue avec la navigation principale, incluant des liens vers l'accueil, les fiches de poste, la génération de quiz, la liste des quiz et les résultats, ainsi qu'une fonction de déconnexion.

---
+++ front/src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <header class="fixed top-3 left-1/2 -translate-x-1/2 mx-auto max-w-6xl w-full z-50 bg-gray-800 text-white px-8 py-4 flex items-center justify-between shadow rounded-full">
+        <div class="text-xl font-bold">QuizMaster Entreprise</div>
+        <nav class="flex items-center gap-4">
+            <router-link to="/">
+                <p class="text-white hover:text-gray-300">Accueil</p>
+            </router-link>
+            <router-link to="/fiches" class="hover:underline">
+                <p class="text-white hover:text-gray-300">Fiches de poste</p>
+            </router-link>
+            <router-link to="/quiz" class="hover:underline">
+                <p class="text-white hover:text-gray-300">Générer un quiz</p>
+            </router-link>
+            <router-link to="/quiz-list" class="hover:underline">
+                <p class="text-white hover:text-gray-300">Liste des quiz</p>
+            </router-link>
+            <router-link to="/resultats" class="hover:underline">
+                <p class="text-white hover:text-gray-300">Résultats</p>
+            </router-link>
+            <button @click="logout" class="text-white hover:text-gray-300">
+                <p>Déconnexion</p>
+            </button>
+        </nav>
+    </header>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function logout() {
+    localStorage.removeItem('token')
+    router.push('/login')
+}
+</script>
