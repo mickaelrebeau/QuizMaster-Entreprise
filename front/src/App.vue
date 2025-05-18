3a01921c93d1359843853c10bd0e117624f0bbe7
Mickael Rebeau <mrebeau@productlife-group.com>
2025-05-18 19:14:30 +0200

    Ajout d'une structure de mise en page dans App.vue, intégrant un composant Header et un conteneur principal pour le router-view, améliorant ainsi l'organisation de l'interface utilisateur.

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,3 +1,14 @@
 <template>
-  <router-view />
+  <div class="w-full h-full flex flex-col">
+    <Header />
+    <main class="flex-1">
+      <div class="max-w-4xl w-full mx-auto px-4 py-8">
+        <router-view />
+      </div>
+    </main>
+  </div>
 </template>
+
+<script setup>
+import Header from './components/Header.vue'
+</script>
