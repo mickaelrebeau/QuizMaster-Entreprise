759c773a23a624875c8621938fc039e77dfba5d5
Mickael Rebeau <mrebeau@productlife-group.com>
2025-05-31 15:25:13 +0200

    Ajout d'un nouveau composant AuthLayout.vue pour structurer la mise en page de l'interface d'authentification, incluant une barre d'application et un conteneur principal pour le router-view.

---
+++ front/src/layouts/AuthLayout.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-layout>
+        <v-app-bar app class="px-10">
+            <div class="d-flex align-center justify-center w-full">
+                <span style="color: #eceff1" class="app-bar-text mx-3 text-h5 font-bold">
+                    QuizMaster Entreprise
+                </span>
+            </div>
+        </v-app-bar>
+
+        <v-main>
+            <router-view />
+        </v-main>
+    </v-layout>
+</template>
+
+<script setup>
+</script>
