68ae92fb1ce205cbb5dcd8493f6d1e5704e106f8
Mickael Rebeau <mrebeau@productlife-group.com>
2025-05-31 15:25:18 +0200

    Ajout d'un nouveau composant MainLayout.vue pour structurer la mise en page principale de l'application, incluant un en-tête et un conteneur principal pour le router-view, améliorant ainsi l'organisation de l'interface utilisateur.

---
+++ front/src/layouts/MainLayout.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-layout>
+        <Header />
+
+        <v-main class="main-content">
+            <router-view />
+        </v-main>
+    </v-layout>
+</template>
+
+<script setup>
+import Header from '../components/Header.vue';
+</script>
+
+<style scoped>
+.main-content {
+    min-height: calc(100vh - 128px);
+    padding: 20px;
+}
+</style>
