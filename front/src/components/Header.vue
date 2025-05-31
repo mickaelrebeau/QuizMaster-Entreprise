57707a9a874009e57d08a6d10166368e1f1233d8
Mickael Rebeau <mrebeau@productlife-group.com>
2025-05-31 15:24:54 +0200

    Modification du composant Header.vue pour changer le style de l'en-tête et mettre à jour les couleurs des liens de navigation, améliorant ainsi l'esthétique et la lisibilité de l'interface utilisateur.

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -1,21 +1,22 @@
 <template>
-    <header class="fixed top-3 left-1/2 -translate-x-1/2 mx-auto max-w-6xl w-full z-50 bg-gray-800 text-white px-8 py-4 flex items-center justify-between shadow rounded-full">
+    <header
+        class="fixed top-3 left-1/2 -translate-x-1/2 mx-auto max-w-6xl w-full z-50 bg-gray-200 text-black px-8 py-4 flex items-center justify-between shadow rounded-full">
         <div class="text-xl font-bold">QuizMaster Entreprise</div>
         <nav class="flex items-center gap-4">
-            <router-link to="/">
-                <p class="text-white hover:text-gray-300">Accueil</p>
+            <router-link to="/dashboard">
+                <p class="text-black hover:text-gray-600">Accueil</p>
             </router-link>
             <router-link to="/fiches" class="hover:underline">
-                <p class="text-white hover:text-gray-300">Fiches de poste</p>
+                <p class="text-black hover:text-gray-600">Fiches de poste</p>
             </router-link>
             <router-link to="/quiz" class="hover:underline">
-                <p class="text-white hover:text-gray-300">Générer un quiz</p>
+                <p class="text-black hover:text-gray-600">Générer un quiz</p>
             </router-link>
             <router-link to="/quiz-list" class="hover:underline">
-                <p class="text-white hover:text-gray-300">Liste des quiz</p>
+                <p class="text-black hover:text-gray-600">Liste des quiz</p>
             </router-link>
             <router-link to="/resultats" class="hover:underline">
-                <p class="text-white hover:text-gray-300">Résultats</p>
+                <p class="text-black hover:text-gray-600">Résultats</p>
             </router-link>
             <button @click="logout" class="text-white hover:text-gray-300">
                 <p>Déconnexion</p>
