452213ddd80086d7742f350a05ab210dd6b0cb8f
Mickael Rebeau <mrebeau@productlife-group.com>
2025-06-09 20:32:41 +0200

    Ajout d'un nouveau composant SimpleLayout.vue : création d'une structure de mise en page avec un conteneur principal et un espace pour le rendu des routes, ainsi que des styles pour assurer une présentation cohérente.

---
+++ front/src/layouts/SimpleLayout.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-layout>
+        <v-main>
+            <router-view />
+        </v-main>
+    </v-layout>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.main-content {
+    min-height: 100vh;
+    padding: 20px;
+}
+</style>
